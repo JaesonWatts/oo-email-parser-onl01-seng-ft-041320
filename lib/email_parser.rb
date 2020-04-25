59c205c227768abcd9b15d645403a66af3e8e101
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-25 05:10:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,10 +14,10 @@ class EmailParser
   end
   
 
-  def parse(csv_data)
-    rows = csv_data.split(", ")
-    emails = rows.collect do |row|
-      
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
 
   
